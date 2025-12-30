67f7bef312cca7ccc0617fdcf58dea8bf590dd52
lukagbn <luka68690@gmail.com>
2025-12-30 21:56:23 +0400

    add products page

---
+++ src/app/products/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+function Page() {
+  const [product, setProduct] = useState(null);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((result) => setProduct(result));
+  }, []);
+  if (product === null) {
+    return (
+      <h2 className={styles.loadingData}>products loading, please wait...</h2>
+    );
+  }
+  return (
+    <section className={styles.cardContainer}>
+      {product?.map((item) => (
+        <div key={item.id} className={styles.card}>
+          <Image src={item.image} width={150} height={150} alt={item.title} />
+          <div className={styles.cardBody}>
+            <p className={styles.location}>Ships to ukraine</p>
+            <h3>{item.title}</h3>
+            <Link href={`/products/${item.id}`}>
+              {" "}
+              <h2>${item.price}</h2>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+}
+
+export default Page;
